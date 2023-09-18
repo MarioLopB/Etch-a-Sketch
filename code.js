4dd0aee8354544a601bd77b5f8f2ab059159c45e
MarioLopB <mlopeb04@estudiantes.unileon.es>
2023-09-18 22:14:58 +0200

    Make grid and rainbow buttons work

--- code.js
+++ code.js
@@ -4,9 +4,12 @@ const grid_size = document.getElementById('grid_size');
 const background_color = document.getElementById('grid_color');
 const pen_color = document.getElementById('pen_color');
 const grid_lines_btn = document.getElementById('grid_lines_btn');
+const clear_btn = document.getElementById('clear_btn');
+const rainbow_btn = document.getElementById('rainbow_btn');
 
 let isClicked = false;
-let isLinesPulsed = true;
+let isLinesPulsed = false;
+let isRainbowPulsed = false;
 
 function createGrid() {
     console.log(typeof grid_size.value);
@@ -26,10 +29,20 @@ function createGrid() {
         const gridItems = document.querySelectorAll('.grid-item');
         gridItems.forEach((gridItem) => {
             console.log(background_color.value);
+            if(isLinesPulsed){
+                gridItem.style.border = "1px solid #ddd";
+            } else {
+                gridItem.style.border = "none";
+            }
+
             gridItem.style.backgroundColor = background_color.value;
             gridItem.addEventListener('mouseover', () => {
                 if (isClicked) {
-                    gridItem.style.backgroundColor = pen_color.value;
+                    if (isRainbowPulsed) {
+                        gridItem.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+                    } else {
+                        gridItem.style.backgroundColor = pen_color.value;
+                    }
                 }
             });
         });
@@ -61,25 +74,39 @@ grid_size.addEventListener('input', (e) => {
     }
 });
 
-background_color.addEventListener('input', (e) => {
+grid_lines_btn.addEventListener('click', (e) => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    if (!isLinesPulsed) {
+        gridItems.forEach((gridItem) => {
+            gridItem.style.border = "1px solid #ddd";
+        });
+        grid_lines_btn.style.cssText = "background-color: #4CAF50; color: white;";
+        isLinesPulsed = true;
+    } else {
+        gridItems.forEach((gridItem) => {
+            gridItem.style.border = "none";
+        });
+        grid_lines_btn.style.cssText = "background-color: while; color: black;";
+        isLinesPulsed = false;
+    }
+
+});
+
+clear_btn.addEventListener('click', (e) => {
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach((gridItem) => {
         gridItem.style.backgroundColor = background_color.value;
     });
 });
 
-grid_lines_btn.addEventListener('click', (e) => {
+rainbow_btn.addEventListener('click', (e) => {
     const gridItems = document.querySelectorAll('.grid-item');
-    if(isLinesPulsed){
-        gridItems.forEach((gridItem) => {
-            gridItem.style.border = "none";
-        });
-        isLinesPulsed = false;
+    if (!isRainbowPulsed) {
+        rainbow_btn.style.cssText = "background-color: #4CAF50; color: white;";
+        isRainbowPulsed = true;
     } else {
-        gridItems.forEach((gridItem) => {
-            gridItem.style.border = "1px solid #ddd";
-        });
-        isLinesPulsed = true;
+        rainbow_btn.style.cssText = "background-color: while; color: black;";
+        isRainbowPulsed = false;
     }
-    
+
 });
